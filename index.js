1654f411d5bb50b4ba5c823cedc6d695872e694e
mattlep11 <103292287+mattlep11@users.noreply.github.com>
2023-12-18 17:33:52 -0500

    Added basic drawing functionality

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const GRID = document.getElementById("grid-container");
 const CELLS = document.querySelectorAll(".grid-cell");
+let currentColour = "black";
+let drawing = false;
+
+CELLS.forEach(c => c.addEventListener('mousedown', () => {
+  c.style.backgroundColor = currentColour;
+  drawing = true;
+}));
+CELLS.forEach(c => c.addEventListener('mousemove', () => {
+  if (drawing)
+    c.style.backgroundColor = currentColour;
+}));
+CELLS.forEach(c => c.addEventListener('mouseup', () => {
+  drawing = false;
+}));
 
 // dynamically sets up the new grid arrangement based on the number of cells
 function initGrid(nbCellsPerRow = 4) {
